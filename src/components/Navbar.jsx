08569afd0e41203ebfd9b917cd403a1a76a7c629
zaidsayed910 <zaidsayed910@gmail.com>
2024-11-10 14:32:29 +0530

    Import navbar.css changes to Navbar.css in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import './navbar.css'
+import './Navbar.css'
 
 const Navbar = () => {
     const [isLoggedIn, setisLoggedIn] = useState(false)
